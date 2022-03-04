e83de6b2a9f45a9a3f83a6db89dcc3302e83ab88
Tal Spektor <talspektor@gmail.com>
2022-03-04 23:42:59 +0200

    refactor test

--- test/network_layer_test.dart
+++ test/network_layer_test.dart
@@ -16,22 +16,17 @@ void main() {
     final postList = PostList.empty();
     final post = PostResponse.empty();
 
-    try {
-      final response = await session.request<PostList>(
-          httpRequest: postRequest, responseType: postList);
-      switch (response.resultEnum) {
-        case ResultEnum.success:
-          final PostList posts = response.resultEnum.value<PostList>(response);
-          // final posts = response.success!.list;
-          print(posts.list);
-          break;
-        case ResultEnum.failure:
-          final Failure failure = response.resultEnum.value<PostList>(response);
-          print(failure.message);
-          break;
-      }
-    } catch (e) {
-      print(e);
+    final response = await session.request<PostList>(
+        httpRequest: postRequest, responseType: postList);
+    switch (response.resultEnum) {
+      case ResultEnum.success:
+        final PostList posts = response.resultEnum.value<PostList>(response);
+        print(posts.list);
+        break;
+      case ResultEnum.failure:
+        final Failure failure = response.resultEnum.value<PostList>(response);
+        print(failure.message);
+        break;
     }
   });
 }
